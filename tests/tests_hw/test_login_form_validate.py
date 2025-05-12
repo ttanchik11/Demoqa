faefcf9903a3216de7aa3764f91b80e1376197fd
TatianaKasatkina <ttanchik11@yandex.ru>
2025-05-12 19:46:12 +0300

    промежуточно

--- tests/tests_hw/test_login_form_validate.py
+++ tests/tests_hw/test_login_form_validate.py
@@ -9,7 +9,7 @@ def test_login_form(browser):
     assert form_page.first_name.get_dom_attribute('placeholder') == 'First Name'
     assert form_page.last_name.get_dom_attribute('placeholder') == 'Last Name'
     assert form_page.user_email.get_dom_attribute('placeholder') == 'name@example.com'
-    assert form_page.user_email.get_dom_attribute('pattern') == "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+    assert form_page.user_email.get_dom_attribute('pattern') == '^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$'
     form_page.btn_submit.click_force()
     assert form_page.form_novalidate.get_dom_attribute('class') == 'was-validated'
 
