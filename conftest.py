11da3cd8cdb860da6c5adfa5d0b5fde3f66c7f8a
TatianaKasatkina <ttanchik11@yandex.ru>
2025-05-23 11:32:07 +0300

    Remove pytest cache

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 @pytest.fixture(scope="session")
 def browser():
     driver = webdriver.Chrome()
-    driver.set_window_size(1000, 1000)
+    driver.set_window_size(1000, 1000)  # Исправлен синтаксис
     yield driver
-    # browser.set_window_size(1000, 1000)
     driver.quit()
